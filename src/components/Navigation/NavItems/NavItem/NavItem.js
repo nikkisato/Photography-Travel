c60896df2aa1a880c4ee33de30620224c8f7036a
Nikki <nikki.satopdx@gmail.com>
2020-03-20 16:38:25 -0700

    finished the navbar

--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -11,23 +11,42 @@ const StyledNavLink = styled(NavLink)`
   display: flex;
   text-transform: uppercase;
   align-items: center;
-  border-bottom: 2px solid transparent;
+  border-bottom: ${props =>
+    props.mobile ? '1px solid transparent' : '1px solid transparent'};
   font-size: 1.2rem;
-  padding: 1rem;
-  margin: 0 1rem;
+  padding: ${props => (props.mobile ? '.5rem 1rem' : '1rem')};
+  margin: ${props => (props.mobile ? '2rem 0' : '0 1rem')};
   font-weight: 400;
   color: var(--color-white);
   transition: all 0.2s;
 
   &:hover {
-    border-bottom: 2px solid var(--color-white);
+    border-bottom: ${props =>
+      props.mobile
+        ? ' 1px solid var(--color-white);'
+        : ' 1px solid var(--color-white)'};
+  }
+
+  &.active {
+    border-bottom: ${props =>
+      props.mobile
+        ? ' 1px solid var(--color-white);'
+        : ' 1px solid var(--color-white)'};
   }
 `;
 
-const NavItem = ({ link, children }) => {
+const NavItem = ({ link, children, mobile, clicked }) => {
   return (
     <Li>
-      <StyledNavLink to={link}>{children}</StyledNavLink>
+      <StyledNavLink
+        onClick={clicked}
+        exact
+        activeClassName='active'
+        mobile={mobile}
+        to={link}
+      >
+        {children}
+      </StyledNavLink>
     </Li>
   );
 };
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import NavItems from '../NavItems/NavItems';
 
@@ -9,11 +9,13 @@ const FixedWrapper = styled.div`
   position: fixed;
   background-color: var(--color-main);
   padding: 0rem 2rem;
+  z-index: 10;
   top: 0;
   left: 0;
   width: 100%;
   height: 6rem;
   display: none;
+
   @media ${props => props.theme.mediaQueries.smallest} {
     display: flex;
   }
@@ -25,14 +27,42 @@ const Wrapper = styled.div`
   align-items: center;
   width: 100%;
 `;
+
+const Menu = styled.div`
+  width: 100%;
+  background-color: var(--color-mainDark);
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 6rem;
+  visibility: ${props => (props.opened ? 'visible' : 'hidden')};
+  transform: translateY(${props => (props.opened ? '0%' : '-100%')});
+  transition: all 0.2s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  display: none;
+
+@media ${props => props.theme.mediaQueries.smallest} {
+  display: flex;
+}
+`;
+
 const SideDrawer = () => {
+  const [isOpened, setIsOpened] = useState(false);
   return (
-    <FixedWrapper>
-      <Wrapper>
-        <Logo />
-        <Hamburger />
-      </Wrapper>
-    </FixedWrapper>
+    <>
+      <FixedWrapper>
+        <Wrapper>
+          <Logo />
+          <Hamburger opened={isOpened} clicked={() => setIsOpened(!isOpened)} />
+        </Wrapper>
+      </FixedWrapper>
+      <Menu opened={isOpened}>
+        <NavItems mobile clicked={() => setIsOpened(false)} />
+      </Menu>
+    </>
   );
 };
 
