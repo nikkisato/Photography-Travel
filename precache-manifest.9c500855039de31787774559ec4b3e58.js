7b50ac9919a0e544ab204081a151e0928b9c358e
Nikki <nikki.satopdx@gmail.com>
2020-03-26 22:28:36 -0700

    Updates

---
+++ precache-manifest.9c500855039de31787774559ec4b3e58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae4fda16bb843f8c01913fbbde0b2f0",
+    "url": "/Photography-Travel/index.html"
+  },
+  {
+    "revision": "cf614538104a730393af",
+    "url": "/Photography-Travel/static/js/2.f45f208f.chunk.js"
+  },
+  {
+    "revision": "53058820f88b1198dd40fa8380b7c312",
+    "url": "/Photography-Travel/static/js/2.f45f208f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac773d6564abe3de535",
+    "url": "/Photography-Travel/static/js/main.15a5809c.chunk.js"
+  },
+  {
+    "revision": "cccf3f624a78b55aea39",
+    "url": "/Photography-Travel/static/js/runtime-main.5579f1c0.js"
+  }
+]);
